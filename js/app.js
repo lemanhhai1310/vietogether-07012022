387bf00453ebe07791e726983af2eca6e689c7f4
Hai LE Manh <hai.lemanh.1310@gmail.com>
2022-01-11 01:55:16 +0700

    update

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@ console.log(
   "%c" + "W E L C O M E   T O   C V . C A T H T M L C S S . N E T",
   "font-family:Montserrat ; font-size:14px; background: #009999; border-radius: 2px; padding: 6px 12px; margin: 5px 10px 7px 0px; color: #ffffff;"
 );
+
+$(window).on('load', function () {
+
+    UIkit.util.on('.header__center__formSearchDropdown', 'show', function () {
+        // do something
+        $('.mask_menu').fadeIn(300);
+    });
+
+    UIkit.util.on('.header__center__formSearchDropdown', 'hide', function () {
+        // do something
+        $('.mask_menu').stop(true, true).fadeOut(300);
+    });
+});
--- template-parts/layouts/header.php
+++ template-parts/layouts/header.php
@@ -22,11 +22,123 @@
 <?php require "template-parts/commons/facebook_chat.php"; ?>
 <!--app-->
 <div id="app" class="uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
-<div>
+<div class="header__bannderAds uk-position-z-index uk-position-relative">
     <div class="uk-container uk-padding-remove">
         <a href=""><img class="uk-width-1-1" src="images/BannerAds.png" alt=""></a>
     </div>
 </div>
 <div class="header">
+    <div class="header__top uk-visible@m">
+        <div class="uk-container uk-padding-remove">
+            <nav class="uk-navbar uk-navbar-container uk-navbar-transparent" uk-navbar>
 
+                <div class="uk-navbar-left">
+                    <div class="uk-navbar-item">
+                        <a href="" class="header__top__btn header__top__btn--store uk-button uk-button-primary"><span>Ghé Thăm Vietogethers Store</span></a>
+                    </div>
+                </div>
+
+                <div class="uk-navbar-right">
+                    <a class="uk-navbar-item header__top__social header__top__social--facebook"></a>
+                    <a class="uk-navbar-item header__top__social header__top__social--instagram"></a>
+                    <a class="uk-navbar-item header__top__social header__top__social--youtube"></a>
+                </div>
+
+            </nav>
+        </div>
+    </div>
+    <div class="header__center uk-background-default uk-position-z-index uk-position-relative">
+        <div class="uk-container uk-padding-remove">
+            <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+                <div class="uk-navbar-left">
+                    <a class="uk-navbar-toggle header__center__iconMenu uk-hidden@m" uk-navbar-toggle-icon href=""></a>
+                    <a href="" class="uk-navbar-item uk-logo uk-hidden@m uk-padding-remove"><img src="images/Vietogetherlogo.png" alt=""></a>
+                    <div class="uk-navbar-item uk-visible@m">
+                        <div class="header__center__linkTxt">Wed, August 25, 2021</div>
+                    </div>
+                    <div class="uk-navbar-item uk-visible@m">
+                        <a href="" class="header__center__linkTxt">Hợp Tác Truyền Thông</a>
+                    </div>
+                </div>
+                <div class="uk-navbar-center">
+                    <a href="" class="uk-navbar-item uk-logo uk-visible@m"><img src="images/Vietogetherlogo.png" alt=""></a>
+                </div>
+                <div class="uk-navbar-right">
+                    <div>
+                        <a class="uk-navbar-toggle uk-hidden@m" href="#" uk-search-icon></a>
+                        <div class="uk-navbar-dropdown header__center__formSearchDropdown" uk-drop="mode: click; cls-drop: uk-navbar-dropdown; boundary: !nav">
+
+                            <div class="uk-grid-small uk-flex-middle" uk-grid>
+                                <div class="uk-width-expand">
+                                    <form class="uk-search uk-search-navbar uk-width-1-1">
+                                        <input class="uk-search-input" type="search" placeholder="Search" autofocus>
+                                    </form>
+                                </div>
+                                <div class="uk-width-auto">
+                                    <a class="uk-navbar-dropdown-close" href="#" uk-close></a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <a class="uk-navbar-toggle uk-hidden@m" uk-icon="icon: user" href="#"></a>
+                    <a class="uk-navbar-toggle uk-hidden@m" uk-icon="icon: sign-in" href="#"></a>
+                    <div class="uk-navbar-item uk-visible@m">
+                        <a href="" class="header__center__linkTxt header__center__linkTxt--register">Đăng Ký</a>
+                    </div>
+                    <div class="uk-navbar-item uk-visible@m">
+                        <a href="" class="header__top__btn uk-button uk-button-primary"><span>Đăng Nhập</span></a>
+                    </div>
+                    <a class="uk-navbar-toggle header__center__iconMenu uk-visible@m" uk-navbar-toggle-icon href=""></a>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <div class="header__bottom uk-visible@m">
+        <div class="uk-container uk-padding-remove">
+            <nav class="uk-navbar uk-navbar-container uk-navbar-transparent" uk-navbar>
+
+                <div class="uk-navbar-left">
+
+                    <ul class="uk-navbar-nav">
+                        <li class="uk-active"><a href="#">Kinh Doanh</a></li>
+                        <li><a href="#">Đời Sống</a></li>
+                        <li><a href="#">Du Lịch</a></li>
+                        <li><a href="#">Sáng Tạo</a></li>
+                        <li><a href="#">Trải Nghiệm</a></li>
+                        <li><a href="#">Video</a></li>
+                        <li><a href="#">Podcast</a></li>
+                        <li><a href="#">Cafe Vietogethers</a></li>
+                    </ul>
+
+                </div>
+
+                <div class="uk-navbar-right">
+                    <div class="uk-navbar-item">
+                        <form class="uk-search header__bottom__formSearch">
+                            <span uk-search-icon></span>
+                            <input class="uk-search-input" type="search" placeholder="Search">
+                        </form>
+                    </div>
+                    <div class="uk-navbar-item">
+                        <div uk-form-custom="target: > * > span:first-child">
+                            <select>
+                                <option value="">Tiếng Việt</option>
+                                <option value="1">Option 01</option>
+                                <option value="2">Option 02</option>
+                                <option value="3">Option 03</option>
+                                <option value="4">Option 04</option>
+                            </select>
+                            <button class="header__bottom__selectLang uk-button uk-button-default" type="button" tabindex="-1">
+                                <span></span>
+                                <span class="uk-position-center-right" uk-icon="icon: chevron-down"></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+            </nav>
+        </div>
+    </div>
 </div>
+<div class="uk-overlay uk-overlay-primary mask_menu"></div>
