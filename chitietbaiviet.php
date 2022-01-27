250998ccc1fc03ed977081ff19ccb66baa97ecba
Hai LE Manh <hai.lemanh.1310@gmail.com>
2022-01-27 16:52:49 +0700

    update

---
+++ chitietbaiviet.php
@@ -0,0 +1,9 @@
+<?php $data["title"] = "CHI TIẾT BÀI VIẾT"; ?>
+<?php $body = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="chitietbaiviet__section uk-section" uk-height-viewport="offset-top: true;offset-bottom: true">
+    <div class="uk-container">
+        
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
