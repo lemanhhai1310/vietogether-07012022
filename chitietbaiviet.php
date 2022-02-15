472e5e39b9c3173d1e26b588ca3b20d93366e62c
Hai LE Manh <hai.lemanh.1310@gmail.com>
2022-02-15 17:02:55 +0700

    update

--- chitietbaiviet.php
+++ chitietbaiviet.php
@@ -10,7 +10,60 @@
             <a href=""><img class="uk-width-1-1" src="images/ADS1.png" alt=""></a>
         </div>
         <div class="chitietbaiviet__item1">
+            <div class="chitietbaiviet__comment__item1">
+                <div class="chitietbaiviet__comment__item2">
+                    <div class="uk-child-width-auto uk-flex-middle" uk-grid>
+                        <div>
+                            <div class="chitietbaiviet__comment__title">Chia Sẻ Bài Viết:</div>
+                        </div>
+                        <div>
+                            <div class="uk-child-width-auto uk-grid-small" uk-grid>
+                                <div>
+                                    <a href="" class="chitietbaiviet__comment__social"><span class="chitietbaiviet__comment__social__icon chitietbaiviet__comment__social__icon--facebook"></span></a>
+                                </div>
+                                <div>
+                                    <a href="" class="chitietbaiviet__comment__social"><span class="chitietbaiviet__comment__social__icon chitietbaiviet__comment__social__icon--skype"></span></a>
+                                </div>
+                                <div>
+                                    <a href="" class="chitietbaiviet__comment__social"><span class="chitietbaiviet__comment__social__icon chitietbaiviet__comment__social__icon--twiter"></span></a>
+                                </div>
+                                <div>
+                                    <a href="" class="chitietbaiviet__comment__social"><span class="chitietbaiviet__comment__social__icon chitietbaiviet__comment__social__icon--youtube"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="chitietbaiviet__comment__item2">
+                    <div class="chitietbaiviet__comment__title chitietbaiviet__comment__title--medium">3 Bình Luận</div>
+                </div>
+                <div class="chitietbaiviet__comment__item2">
+                    <div class="uk-grid-10 uk-grid-20-m" uk-grid>
+                        <div class="uk-width-auto">
+                            <div class="uk-cover-container uk-border-circle">
+                                <img src="images/lemanhhai_anhthe.jpg" alt="" uk-cover="">
+                                <canvas width="83" height="83"></canvas>
+                            </div>
+                        </div>
+                        <div class="uk-width-expand">
+                            <div class="chitietbaiviet__comment__box1">
+                                <div class="uk-child-width-auto" uk-grid>
+                                    <div>
+                                        <a href="">Hà Tùng</a>
+                                    </div>
+                                    <div>
+                                        <a href="">Trả Lời</a>
+                                    </div>
+                                </div>
+                                <div>just a nice post</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="chitietbaiviet__comment__item1">
 
+            </div>
         </div>
     </div>
 </div>
