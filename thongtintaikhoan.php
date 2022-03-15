8ad8ef3737cc9b7068c49b715bc2d92d95175f5a
hailm <hai.lemanh.1310@gmail.com>
2022-03-15 23:28:17 +0700

    update page thongtintaikhoan.php

---
+++ thongtintaikhoan.php
@@ -0,0 +1,9 @@
+<?php $data["title"] = "TÀI KHOẢN"; ?>
+<?php $body = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-section thongtintaikhoan__section">
+    <div class="uk-container">
+
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
