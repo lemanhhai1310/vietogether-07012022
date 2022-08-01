a807aa78a933f0af331a811b7046d7a68a78d889
hailm <hai.lemanh.1310@gmail.com>
2022-08-01 18:09:17 +0700

    update

---
+++ podcast.php
@@ -0,0 +1,9 @@
+<?php $data["title"] = "PODCAST"; ?>
+<?php $body = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-section">
+    <div class="uk-container">
+
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
